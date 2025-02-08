f8ec0b17140e13ad65d8f99fe5fb8325457c3dcf
victor-capriles <vdcapriles@gmail.com>
2025-02-09 00:14:09 +0100

    slight system prompt changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from langchain_community.vectorstores import Chroma
 from langchain_ollama import ChatOllama
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 import chainlit as cl
 
@@ -21,7 +20,7 @@ recursive_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overla
 chunked_documents = recursive_splitter.split_documents(documents)
 
 # initialize the model using HuggingFaceEmbeddings wrapper
-embedding_model = HuggingFaceEmbeddings(model_name="all-minilm-l6-v2")
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/multi-qa-mpnet-base-dot-v1")
 
 # Create chroma vector database from documents
 vectordb = Chroma.from_documents(
@@ -33,12 +32,14 @@ vectordb = Chroma.from_documents(
 @cl.on_chat_start
 async def on_chat_start():
     # declare the mode
-    llm = ChatOllama(model="llama3.2", temperature=0.5, streaming=True)
+    llm = ChatOllama(model="llama3.2", streaming=True)
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system",
-             "You are a refined and courteous butler. Your responses are always polite and respectful additionally,"
-             "you must answer any question with a touch of sophistication. **Don't** make up information. "),
+             "You are a refined and courteous butler. Your responses are always polite and respectful with a touch of sophistication,"
+             "Your task is:"
+              "1) When the user's question is directly related to Victor Capriles or his work, use the provided context to answer."
+              "2) When the question is general or unrelated to Victor Capriles, ignore the context and answer based solely on the query. "),
             ("human", "{question}")
         ]
     )
@@ -52,8 +53,9 @@ async def on_message(message: cl.Message):
 
     question = message.content
 
-    retrieved_docs = vectordb.similarity_search(question, k=3)
+    retrieved_docs = vectordb.similarity_search(question, k=8)
     context = "\n".join([doc.page_content for doc in retrieved_docs])
+    print(retrieved_docs)
 
     # Augment the user's question with the retrieved context
     augmented_input = f"Context:\n{context}\n\nQuestion: {question}"
